f181b864a3524219e0acc04da42b3f697d9d4003
Lainey <laineycreighton@laineys-mbp.mynetworksettings.com>
2023-11-10 18:24:04 -0500

    add question and answer for arithmetic four

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-//---------- ADD ----------//
+//********** ARITHMIC **********//
+
+//----- ADD -----//
 
 // VARIABLES //
 var num1 = 5;
@@ -7,12 +9,10 @@ var num2 = 10;
 // FUNCTION //
 function add() {
     var sum = num1 + num2;
+    console.log("Addition:");
     console.log(sum);
     return sum;
 };
-
-// CALLING THE FUNCITON //
-add(num1, num2);
 
 // DISPLAY ANSWER //
 const arithmetic1 = document.getElementById('arithmetic1');
@@ -20,7 +20,7 @@ arithmetic1.textContent = add();
 
 
 
-//---------- SUBTRACT ----------//
+//----- SUBTRACT -----//
 
 // VARIABLES //
 var num1 = 50;
@@ -29,12 +29,10 @@ var num2 = 10;
 // FUNCTION //
 function subtract() {
     var difference = num1 - num2;
+    console.log("Subtraction:");
     console.log(difference);
     return difference;
 };
-
-// CALLING THE FUNCITON //
-subtract(num1, num2);
 
 // DISPLAY ANSWER //
 const arithmetic2 = document.getElementById('arithmetic2');
@@ -42,7 +40,7 @@ arithmetic2.textContent = subtract();
 
 
 
-//---------- MULTIPLY ----------//
+//----- MULTIPLY -----//
 
 // VARIABLES //
 var num1 = 25;
@@ -51,13 +49,31 @@ var num2 = 4;
 // FUNCTION //
 function multiply() {
     var product = num1 * num2;
+    console.log("Multiplication:");
     console.log(product);
     return product;
 };
 
-// CALLING THE FUNCITON //
-multiply(num1, num2);
-
 // DISPLAY ANSWER //
 const arithmetic3 = document.getElementById('arithmetic3');
 arithmetic3.textContent = multiply();
+
+
+
+//----- DIVIDE -----//
+
+// VARIABLES //
+var num1 = 100;
+var num2 = 4;
+
+// FUNCTION //
+function divide() {
+    var quotient = num1 / num2;
+    console.log("Division:");
+    console.log(quotient);
+    return quotient;
+};
+
+// DISPLAY ANSWER //
+const arithmetic4 = document.getElementById('arithmetic4');
+arithmetic4.textContent = divide();

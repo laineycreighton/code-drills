e90605ba6ae06c40b10732d81e477c966711899e
Lainey Creighton <laineycreighton@laineys-mbp.mynetworksettings.com>
2023-11-10 16:15:04 -0500

    add question and answer for arithmetic two

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//---------- ARITHMETIC ----------//
+//---------- ADD ----------//
 
 // VARIABLES //
 var num1 = 5;
@@ -17,3 +17,25 @@ add(num1, num2);
 // DISPLAY ANSWER //
 const arithmetic1 = document.getElementById('arithmetic1');
 arithmetic1.textContent = add();
+
+
+
+//---------- SUBTRACT ----------//
+
+// VARIABLES //
+var num1 = 50;
+var num2 = 10;
+
+// FUNCTION //
+function subtract() {
+    var difference = num1 - num2;
+    console.log(difference);
+    return difference;
+};
+
+// CALLING THE FUNCITON //
+add(num1, num2);
+
+// DISPLAY ANSWER //
+const arithmetic2 = document.getElementById('arithmetic2');
+arithmetic2.textContent = subtract();
